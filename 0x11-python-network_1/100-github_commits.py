b671fdb664045a6b78a36b8890ffc8c1b2228574
ChamberlainO <omuchamberlain@gmail.com>
2023-04-01 10:04:54 -0700

    interview

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Uses the GitHub API to display a GitHub ID based on given credentials.
-Usage: ./10-my_github.py <GitHub username> <GitHub password>
-  - Uses Basic Authentication to access the ID.
 """
+list 10 commits (from the most recent to oldest) of the repository and user
+sent in as arguments
+"""
+
 import requests
 import sys
 
