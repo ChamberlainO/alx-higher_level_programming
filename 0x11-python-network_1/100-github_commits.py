685c8e62b0f2c02e031fe5aacf4b42f4a76a81bf
ChamberlainO <omuchamberlain@gmail.com>
2023-04-01 09:31:22 -0700

    Time for an interview

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,12 +3,21 @@
 Usage: ./10-my_github.py <GitHub username> <GitHub password>
   - Uses Basic Authentication to access the ID.
 """
-import sys
 import requests
-from requests.auth import HTTPBasicAuth
+import sys
 
+repo_name = sys.argv[1]
+owner_name = sys.argv[2]
 
-if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    commits = response.json()
+    for commit in commits[:10]:
+        sha = commit["sha"]
+        author_name = commit["commit"]["author"]["name"]
+        print(f"{sha}: {author_name}")
+else:
+    print("Error: Could not retrieve commits")
