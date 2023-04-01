034370b9b3dc4f8f8f2aa9f1c021f4b61abf284d
ChamberlainO <omuchamberlain@gmail.com>
2023-04-01 10:07:19 -0700

    interview

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,21 +4,12 @@ list 10 commits (from the most recent to oldest) of the repository and user
 sent in as arguments
 """
 
-import requests
-import sys
-
-repo_name = sys.argv[1]
-owner_name = sys.argv[2]
-
-url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
-
-response = requests.get(url)
-
-if response.status_code == 200:
-    commits = response.json()
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(argv[2], argv[1]))
+    commits = r.json()
     for commit in commits[:10]:
-        sha = commit["sha"]
-        author_name = commit["commit"]["author"]["name"]
-        print(f"{sha}: {author_name}")
-else:
-    print("Error: Could not retrieve commits")
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
