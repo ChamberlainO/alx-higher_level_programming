19e49eff174eeda08af624328dfa24b3ed4070c9
ChamberlainO <omuchamberlain@gmail.com>
2023-04-01 08:56:09 -0700

    Error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,15 +1,12 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 takes in a URL, sends a request to the URL & displays the body of the response
 """
-import requests
-import sys
-
-url = sys.argv[1]
-
-response = requests.get(url)
-
-if response.status_code >= 400:
-    print("Error code:", response.status_code)
-else:
-    print(response.text)
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    r = requests.get(argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
